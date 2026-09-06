ffb57b7bc74d244d8009e2493234a2919c14e9d1
Abdul Mannan <abdulmannan010spn@gmail.com>
2026-09-06 13:01:06 +0530

    Making Responsive

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function page2animation() {
       start: "top 80%",
       end: "bottom 10%",
       scroller: "body",
-      scrub: true,
+      
     },
   });
 
@@ -106,7 +106,7 @@ function page2animation() {
     {
       x: -100,
       opacity: 0,
-      duration: 1,
+      duration: 1.5,
       ease: "sine.out",
     },
     "anim1",
@@ -117,7 +117,7 @@ function page2animation() {
     {
       x: 100,
       opacity: 0,
-      duration: 1,
+      duration: 1.5,
       ease: "sine.out",
     },
     "anim1",
@@ -128,7 +128,7 @@ function page2animation() {
     {
       x: -100,
       opacity: 0,
-      duration: 1,
+      duration: 1.5,
       ease: "sine.out",
     },
     "anim2",
@@ -139,7 +139,7 @@ function page2animation() {
     {
       x: 100,
       opacity: 0,
-      duration: 1,
+      duration: 1.5,
       ease: "sine.out",
     },
     "anim2",
@@ -150,7 +150,7 @@ function page2animation() {
     {
       x: -100,
       opacity: 0,
-      duration: 1,
+      duration: 1.5,
       ease: "sine.out",
     },
     "anim3",
@@ -161,7 +161,7 @@ function page2animation() {
     {
       x: 100,
       opacity: 0,
-      duration: 1,
+      duration: 1.5,
       ease: "sine.out",
     },
     "anim3",
